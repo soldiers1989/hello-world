eaf2f72d9ec8bc6ca296df811607c26dbfbdd93f
liwei <liwei@yxcoach.com>
2018-06-11 09:03:27 +0800

    update

--- mysql简单操作.sql
+++ mysql简单操作.sql
@@ -3,3 +3,6 @@ CREATE USER 'elppm_user'@'%' IDENTIFIED BY 'elppm';
 
 #授予全部权限
 GRANT ALL ON *.* TO 'elppm_user'@'%'; 
+
+#查看MYSQL数据库中所有用户
+SELECT DISTINCT CONCAT('User: ''',user,'''@''',host,''';') AS query FROM mysql.user;
